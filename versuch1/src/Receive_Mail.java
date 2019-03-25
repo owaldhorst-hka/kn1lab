0d7fe848e1dc6449a06f95d2904fe1cd43f34b85
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-03-25 23:13:31 +0100

    Add template for experiment 1

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -5,58 +5,15 @@ import javax.mail.Session;
 import javax.mail.Store;
 
 public class Receive_Mail {
-	
 	public static void main(String[] args) throws Exception {
 		fetchMail();
-    }
+	}
 	
 	public static void fetchMail() {
-	    // mail server connection parameters
-	    String host = "localhost";
-	    String user = "user";
-	    String password = "password";
-
-	    Properties properties = System.getProperties();
-
-	    // uncomment for debugging
-	 // properties.put("mail.debug", "true");
-	 // properties.put("mail.debug.quote", "true");  
-	    
-	    Session session = Session.getDefaultInstance(properties);
-
-	    // uncomment for debugging
-	 // session.setDebug(true);
-	    
 		try {
-		    // connect to my pop3 inbox
-		    Store store = session.getStore("pop3");
-		    store.connect(host, user, password);
-		    Folder inbox = store.getFolder("Inbox");
-		    inbox.open(Folder.READ_ONLY);
-
-		    // get the list of inbox messages
-		    Message[] messages = inbox.getMessages();
-
-		    if (messages.length == 0) System.out.println("No messages found.");
-
-		    // print all messages
-		    for (int i = 0; i < messages.length; i++) {
-		      System.out.println("Message " + (i + 1));
-		      System.out.println("From : " + messages[i].getFrom()[0]);
-		      System.out.println("To : " + messages[i].getAllRecipients()[0]);
-		      System.out.println("Subject : " + messages[i].getSubject());
-		      System.out.println("Sent Date : " + messages[i].getSentDate());
-		      System.out.println(messages[i].getContent());
-		      System.out.println();
-		    }
-
-		    inbox.close(true);
-		    store.close();
+			// your code here
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
-  
-
-
--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,4 +1,3 @@
-import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -9,46 +8,15 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage; 
 
 public class Send_Mail {
-
-	public static void main(String[] args) {    
-		sendMail();
+	public static void main(String[] args) {
+		sendMail();   
 	}
 	
 	public static void sendMail() {
-		// mail server connection parameters
-	    String host = "localhost";
-	    String user = "user";
-	    String password = "password";
-	    String from = "me@example.com";
-	    String to = "you@localhost";
-	    String subject = "JavaMail hello world example";
-	    String text = "Hello, world!\\n";
-
-	    Properties properties = System.getProperties();
-	    // properties.put("mail.smtp.host", host);
-	    
-	    // uncomment for debugging
-	 // properties.put("mail.debug", "true");
-	 // properties.put("mail.debug.quote", "true");  
-	    
-	    Session session = Session.getDefaultInstance(properties);
-	    
-	    // uncomment for debugging
-	 // session.setDebug(true);
-		
 		try {
-	        MimeMessage msg = new MimeMessage(session);
-	        msg.setFrom(from);
-	        msg.setRecipients(Message.RecipientType.TO, to);
-	        msg.setSubject(subject);
-	        msg.setSentDate(new Date());
-	        msg.setText(text);
-	        Transport.send(msg, user, password);
-	        
-	        System.out.println("Mail sent successfully!");
-
-	   } catch (MessagingException mex) {
-		   System.out.println("Send failed, exception: " + mex);
-		   }
+			// your code here
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+	}
 }
