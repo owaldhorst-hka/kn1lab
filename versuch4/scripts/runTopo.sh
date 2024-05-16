019fc45a50c0e57e0b0a5365043f5081c35f4c08
bekl1011 <106782449+bekl1011@users.noreply.github.com>
2024-05-16 16:43:17 +0200

    Update runTopo.sh scripts

--- versuch4/scripts/runTopo.sh
+++ versuch4/scripts/runTopo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ $1 -eq 1 ]
 then
-sudo /usr/bin/python3 /home/labrat/kn1lab/versuch4/scripts/mininet_1.py
+sudo /usr/bin/python3 "$PWD"/mininet_1.py
 elif [ $1 -eq 2 ]
 then
-sudo /usr/bin/python3 /home/labrat/kn1lab/versuch4/scripts/mininet_2.py
+sudo /usr/bin/python3 "$PWD"/mininet_2.py
 elif [ $1 -eq 3 ]
 then
-sudo /usr/bin/python3 /home/labrat/kn1lab/versuch4/scripts/mininet_3.py
+sudo /usr/bin/python3 "$PWD"/mininet_3.py
 else
 echo "Please give a number from 1 to 3 to select the mininet script you want to execute!"
 fi
